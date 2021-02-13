da6df388e03d5b62a361c3c411436e03caf5192d
42010 <72303709+42010@users.noreply.github.com>
2021-02-13 12:54:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ var SpeechRecognition = window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
 
-function starts() {
+function start() {
     document.getElementById("textbox").innerHTML = "";
     recognition.start();
-}
 
-recognition.onresult = function run(event) {
-    console.log(event);
+    recognition.onresult = function run(event) {
+        console.log(event);
 
-    var Content = event.results[0][0].transcript;
-    console.log(Content);
+        var Content = event.results[0][0].transcript;
+        console.log(Content);
 
-    document.getElementById("textbox").innerHTML = Content;
-    console.log(Content);
-    speak();
+        document.getElementById("textbox").innerHTML = Content;
+        console.log(Content);
+        speak();
+    }
 }
 
 function speak() {
